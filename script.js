9e777cefeb617b2dc628178efa8195b4dd8efe91
ItmeEmir <ehmir656@gmail.com>
2025-03-03 17:52:37 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,2 +1,8 @@
 let fruit = { "Graps", "Dragonfruit", "Banana", "Guava"};
-let fruit = fruits[0]; //
+let fruit = fruits[0];
+function logFirstFruit()
+{
+  let randomNumber= Math.floor(Math.random()*fruits.length);
+  console.log(fruits[]);
+}
+logFristFruit();
